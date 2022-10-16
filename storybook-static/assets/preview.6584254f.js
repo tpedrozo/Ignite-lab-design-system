19e7ef6a89a55e86c03e6576bf3d443aab312ad6
tpedrozo <mr.thiagocesar@hotmail.com>
2022-10-16 02:28:02 -0300

    add checkbox components

--- storybook-static/assets/preview.6584254f.js
+++
@@ -1,2 +0,0 @@
-import{t as o}from"./index.68ae7140.js";import"./iframe.b2ab446c.js";import"./es.map.constructor.583f7a34.js";import"./es.number.to-fixed.719cd426.js";import"./index.1d3c7146.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
-//# sourceMappingURL=preview.6584254f.js.map
---
+++ storybook-static/assets/preview.af6f9fd1.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.cf0ea1db.js";import"./index.c1c43bc6.js";import"./iframe.76f59bbc.js";const a={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{a as parameters};
+//# sourceMappingURL=preview.af6f9fd1.js.map
